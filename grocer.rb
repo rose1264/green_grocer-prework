9c7b281b00b233feb26732e3a59f725cc3e0ea5f
Shumei Huang <rose1264@gmail.com>
2018-06-14 18:37:55 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -38,7 +38,6 @@ end
 def apply_clearance(cart)
   cart.each do |name, properties|
     if properties[:clearance]
-      puts properties[:clearance]
       updated_price = properties[:price] * 0.80
       properties[:price] = updated_price.round(2)
     end
